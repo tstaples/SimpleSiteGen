84db87f6dcff1bff41c9438cdd8c751b9a8419db
tyler staples <tyler.a.staples@gmail.com>
2016-03-12 00:47:55 -0800

    added helper methods to layout for formatting based on indents

--- ssg/layouts.py
+++ ssg/layouts.py
@@ -22,3 +22,23 @@ class Layouts(object):
 				self.layouts[name] = utils.get_file_lines(self.layouts_dir + name)
 			layout = self.layouts[name]
 		return layout
+
+
+	def layout_exists(self, name):
+		return (name in self.layouts)
+
+
+	def get_formatted_layout(self, layout_name, num_tabs):
+		return Layouts.format_layout(self.get_layout(layout_name), num_tabs)
+
+
+	@staticmethod
+	def format_layout(layout_data, num_tabs):
+		formatted_lines = []
+		for line in layout_data:
+			tabs = "".join("\t" for i in range(num_tabs))
+			formatted_lines.append(tabs + line)
+		# add a newline to the last line if there isn't one
+		if not formatted_lines[-1].endswith("\n"):
+			formatted_lines[-1] += "\n"
+		return formatted_lines
